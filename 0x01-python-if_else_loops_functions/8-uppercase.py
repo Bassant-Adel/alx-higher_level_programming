473469eee19e46d49c03073237371cf134baaf6f
Bassant-Adel <baadel404@gmail.com>
2023-10-24 09:45:56 -0700

    ee

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,7 +5,7 @@ def uppercase(str):
     for upper_c in str:
         print("{:c}".format(65 + (ord(upper_c) - 97)
 
-            if ord(upper_c) >= 97 and ord(upper_c) <= 122
+                            if ord(upper_c) >= 97 and ord(upper_c) <= 122
+                            else ord(upper_c)), end="")
 
-            else ord(upper_c)), end="")
     print("")
