899662a86de92d369399dbfc5c6aa951cd8c969b
Bassant-Adel <baadel404@gmail.com>
2024-01-09 12:22:55 -0800

    saa :D

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -5,6 +5,7 @@ module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
+
   print () {
     for (let b = 0; b < this.height; b++) console.log('X'.repeat(this.width));
   }
@@ -12,6 +13,7 @@ module.exports = class Rectangle {
   rotate () {
     [this.width, this.height] = [this.height, this.width];
   }
+
   double () {
     [this.width, this.height] = [this.width * 2, this.height * 2];
   }
